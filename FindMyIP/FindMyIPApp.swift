f83382e1192e824e01b2aa9e5dcc33f67e899a57
alok4508 <aloksingh.itengg@gmail.com>
2024-02-05 09:04:33 +0530

    Initial Commit

---
+++ FindMyIP/FindMyIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyIPApp.swift
+//  FindMyIP
+//
+//  Created by alok singh on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FindMyIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
